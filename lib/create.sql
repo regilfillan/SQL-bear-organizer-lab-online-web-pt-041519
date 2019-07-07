33fd2bbebdcb962db5b201643bc221740e5bcae6
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-07 21:47:48 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,11 @@
-def 
+def create
+  "CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN,
+    );"
+end
